0b40d331cf772e251c2125724dc198854fa9a10a
jawaionline <yudi.kurniawan0306@gmail.com>
2025-04-01 07:11:06 +0700

    Update dailymotion_m3ugrabber.py

--- scripts/dailymotion_m3ugrabber.py
+++ scripts/dailymotion_m3ugrabber.py
@@ -23,7 +23,7 @@ if len(sys.argv) == 2:
                 'https' : sys.argv[1]
               }
 
-na = 'https://www.dailymotion.com/video/x8qckyq'
+na = 'https://raw.githubusercontent.com/jawaionline/dailymotion_to_m3u/main/trans7.m3u'
 def grab(line):
     try:
         _id = line.split('/')[4]
